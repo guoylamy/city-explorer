905e73cb1a3f34c6c13c344694808a7f7c166c23
aaawangyh <wangyh1@shanghaitech.edu.cn>
2021-04-24 00:30:41 +0800

    init_climate_page

--- frontend/src/Climate.js
+++ frontend/src/Climate.js
@@ -1,13 +1,116 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Brightness5Icon from '@material-ui/icons/Brightness5';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-class Climate extends React.Component {
-    render() {
-        return (
-            <div>
-                Climate
-            </div>
-        );
-    }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Climate Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Climate;
+const useStyles = theme => ({
+  root: {
+	height: '100vh',
+  },
+  image: {
+	backgroundImage: 'url(https://source.unsplash.com/random)',
+	backgroundRepeat: 'no-repeat',
+	backgroundColor:
+	  theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+	backgroundSize: 'cover',
+	backgroundPosition: 'center',
+  },
+  paper: {
+	margin: theme.spacing(8, 4),
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+  },
+  avatar: {
+	margin: theme.spacing(1),
+	backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+	width: '100%', // Fix IE 11 issue.
+	marginTop: theme.spacing(1),
+  },
+  submit: {
+	margin: theme.spacing(3, 0, 2),
+  },
+});
+
+class Climate extends React.Component {
+	render() {
+		const { classes } = this.props;
+		return (
+			<Grid container component="main" className={classes.root}>
+			  <CssBaseline />
+			  <Grid item xs={false} sm={4} md={7} className={classes.image} />
+			  <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<div className={classes.paper}>
+				  <Avatar className={classes.avatar}>
+					<Brightness5Icon />
+				  </Avatar>
+				  <Typography component="h1" variant="h5">
+					City Overview
+				  </Typography>
+				  <form className={classes.form} noValidate>
+					<TextField
+					  variant="outlined"
+					  margin="normal"
+					  required
+					  fullWidth
+					  id="city"
+					  label="City"
+					  name="city"
+					  autoComplete="email"
+					  autoFocus
+					/>
+					<TextField
+					  variant="outlined"
+					  margin="normal"
+					  required
+					  fullWidth
+					  name="date"
+					  label="Date"
+					  type="password"
+					  id="date"
+					  autoComplete="current-password"
+					/>
+					<Button
+					  type="submit"
+					  fullWidth
+					  variant="contained"
+					  color="primary"
+					  className={classes.submit}
+					>
+					  City Overview
+					</Button>
+					<Box mt={5}>
+					  <Copyright />
+					</Box>
+				  </form>
+				</div>
+			  </Grid>
+			</Grid>
+		);
+	}
+}
+
+export default  withStyles(useStyles)(Climate);
