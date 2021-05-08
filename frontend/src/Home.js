db7e2445b93e24fbf9d810fa9712fbdf3a7ce6e9
tkong233 <tkong@wesleyan.edu>
2021-05-08 04:37:34 -0400

    added hero to home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import HomePageTabs from './HomePage/HomePageTabs';
 import HomePageCharts from './HomePage/HomePageCharts';
+import './App.css';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
-              <HomePageTabs/>
-              <HomePageCharts/>
+                <div class="hero-image">
+                <div class="hero-text">
+                    <h1>City Explorer</h1>
+                    <p>Find out everything about a city</p>
+                </div>
+                </div>
+                <div style={{margin: '3%'}}>
+                <HomePageCharts/>
+                <div style={{margin: '3%'}}>
+                <HomePageTabs/>
+                </div>
+                </div>
             </div>
         );
     }
