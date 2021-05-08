fb575766ae9e77ceefdf483c9f354cb3ca5a38bf
tkong233 <tkong@wesleyan.edu>
2021-05-08 07:10:01 -0400

    small fix

--- frontend/src/ClimateResult/PrecipitationResult.js
+++ frontend/src/ClimateResult/PrecipitationResult.js
@@ -4,15 +4,46 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 import Title from './Title';
 
 
 export default function PrecipitationResult(row) {
-  const rows = row.data;
+  const rows = row.data.map(({state_name, cnt}) => {
+    return {
+      State: state_name,
+      Count: cnt
+    }
+  });
   return (
     <React.Fragment>
       <Title>Top 10 states with least number of days of precipitation</Title>
-      <Table size="small">
+      <BarChart
+          width={700}
+          height={300}
+          data={rows}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          style={{margin: 'auto'}}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="State" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Count" fill="#8884d8" />
+
+        </BarChart>
+    </React.Fragment>
+  );
+}
+
+{/* <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>State</TableCell>
@@ -27,7 +58,4 @@ export default function PrecipitationResult(row) {
             </TableRow>
           ))}
         </TableBody>
-      </Table>
-    </React.Fragment>
-  );
-}
+      </Table> */}
--- frontend/src/ClimateResult/TemperatureDiffResult.js
+++ frontend/src/ClimateResult/TemperatureDiffResult.js
@@ -4,14 +4,45 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 import Title from './Title';
 
 export default function TemperatureDiffResult(row) {
-  const rows = row.data;
+  const rows = row.data.map(({state, temp}) => {
+    return {
+      State: state,
+      Difference: temp
+    }
+  });
   return (
     <React.Fragment>
       <Title>Top 10 states with smallest average daily temperature difference</Title>
-      <Table size="small">
+      <BarChart
+          width={700}
+          height={300}
+          data={rows}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          style={{margin: 'auto'}}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="State" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Difference" fill="#8884d8" />
+
+        </BarChart>
+    </React.Fragment>
+  );
+}
+
+{/* <Table size="small">
         <TableHead>
           <TableRow>
             <TableCell>State</TableCell>
@@ -26,7 +57,4 @@ export default function TemperatureDiffResult(row) {
             </TableRow>
           ))}
         </TableBody>
-      </Table>
-    </React.Fragment>
-  );
-}
+      </Table> */}
